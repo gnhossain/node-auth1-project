247748015c7a91d1f41eb16a8647cfba5246c0fc
gul hossain <spicymangojungle@gmail.com>
2020-04-20 16:50:54 -0700

    routers

---
+++ api/api-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const authRouter = require('../auth/auth-router');
+const usersRouter = require('../users/users-router');
+
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
+
+router.get('/', (req, res) => {
+  res.json({ api: "It's alive" });
+});
+
+
+
+
+
+module.exports = router;
