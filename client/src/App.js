58364ed35c3a0fb3baee05371858d7f3be44d674
gul hossain <spicymangojungle@gmail.com>
2020-04-20 19:06:29 -0700

    stretch

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SignUp from './components/SignUp.js';
+import Login from './components/Login.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <h2>Hello!</h2>
+      <SignUp/>
+      <Login/>
+    </div>
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [ changes, setChanges] = useState({
+        username: '',
+        password: '',
+    });
+
+    const changeHandler = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setChanges({ ...changes, [name]: value});
+        //console.log(name)
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        const newLogin = {
+            ...changes,
+        }
+
+        axios.post(`http://localhost:5030/api/auth/login`, newLogin)
+        .then(res => console.log(res))
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <form onSubmit = {submitForm}>
+                <label>
+                    Username:
+                    <input type="text" name="username" onChange={changeHandler}/>
+                </label>
+                <label>
+                    Password:
+                    <input type="text" name="password" onChange={changeHandler}/>
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+
+}
+
+export default Login;
